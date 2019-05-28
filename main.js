592515fce022a721e1d3199ea7871df5fda1ad79
007blackkid <roberrto_sx@hotmail.com>
2019-05-27 20:09:09 -0500

    ADD
    LAKSDJ

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var path = require('path');
 var mysql = require('mysql');
+const session = require('express-session');
 
 var con = mysql.createConnection({
   host: "localhost",
--- routes.js
+++ routes.js
@@ -1,29 +1,23 @@
-//const express = require('express');
-//const router= express.Router();
-//const routes = require('./routes');
- 
-
 module.exports=(app, passport)=>{
    
-    app.get('/', (req, res) => {res.send('Hello World!')})
+    app.get('/MisPacientes', (req, res) => {
+            res.render('MisPacientes.ejs')
+        })
     
     app.get('/login', (req, res) => {
         res.render('login.ejs')
-        });
-        
-
-        app.get('/MisPacientes', (req, res) => {
+        });        
+    app.get('/MisPacientes', (req, res) => {
             res.render('MisPacientes.ejs')
-        })
-        app.get('/MisConsultas', (req, res) => {
+    });
+    app.get('/MisConsultas', (req, res) => {
             res.render('MisConsultas.ejs')
-        })
-        app.get('/ExamenesMedicos', (req, res) => {
-            res.render('ExamenesMedicos.ejs')
-        })
-        app.get('/iDOCTOR', (req, res) => {
-            res.render('Home.ejs')
-        })
+    });
+    app.get('/ExamenesMedicos', (req, res) => {
+       res.render('ExamenesMedicos.ejs')
+    });     
+        
+        
 
 }
 
