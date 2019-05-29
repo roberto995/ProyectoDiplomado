97161b132b8470b6891849c82f3b65043a53d178
jhernandez19 <50375122+jhernandez19@users.noreply.github.com>
2019-05-29 11:35:08 -0500

    Update comentarExamen.php

--- PHP/comentarExamen.php
+++ PHP/comentarExamen.php
@@ -4,6 +4,10 @@
 	$comentario = $_GET['comentario'];    
 	$folio = $_GET["folio"];
 	$fecha = date("Y-m-d");
+	
+
 	$insertar = "INSERT INTO comentariosexamenes(folio,comentario,fecha) VALUES ('$folio','$comentario','$fecha')";
+	
 	$con->query($insertar);
+
 ?>
