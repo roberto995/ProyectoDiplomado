d430a8394aa94cc1dc0651529ff8621b6bffe911
jhernandez19 <50375122+jhernandez19@users.noreply.github.com>
2019-05-29 11:35:17 -0500

    Update insertar.php

--- PHP/insertar.php
+++ PHP/insertar.php
@@ -4,15 +4,17 @@ $con = new mysqli('localhost','root','','tfidoctor');
     $mensaje = $_GET['mensaje'];
     $fecha_actual = date("Y-m-d");
     $id = $_GET["persona"];
+    $doctor = $_GET['doctor'];
 
-$insertar = "INSERT INTO historial(Fecha,Comentarios,Id_P) VALUES ('$fecha_actual','$mensaje','$id')";
+$insertar = "INSERT INTO historial(Fecha,Comentarios,Id_P,doctor) VALUES ('$fecha_actual','$mensaje','$id','$doctor')";
 
 $con->query($insertar);
 
 $arreglo = array(
 	'mensaje' => $mensaje, 
     'fecha_actual' => $fecha_actual,
-    'id' => $id
+    'id' => $id,
+    'doctor' => $doctor
 );
 
 echo json_encode($arreglo);
