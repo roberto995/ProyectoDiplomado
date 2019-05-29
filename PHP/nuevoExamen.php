e4e0e2ad43e908de1a97d9bde1b28060c8c0cd40
jhernandez19 <50375122+jhernandez19@users.noreply.github.com>
2019-05-29 11:35:21 -0500

    Update nuevoExamen.php

--- PHP/nuevoExamen.php
+++ PHP/nuevoExamen.php
@@ -5,10 +5,11 @@ $con = new mysqli('localhost','root','','tfidoctor');
 	$examen=$_GET['examen'];
     $nombre=$_GET['nombre'];
     $fecha = $_GET['fecha'];
-    $comentario = '';    
+    $comentario = ''; 
+    $doctor = $_GET['doctor'];
     
 
 $insertar = "INSERT INTO examenes_m(Id_P, Nom_E,Nom_P,Fecha_e,Comentarios_e,status) 
-            VALUES ('1','$examen','$nombre','$fecha','$comentario','Pendiente')";
+            VALUES ('1','$examen','$nombre','$fecha','$comentario','Pendiente','$doctor')";
 $con->query($insertar);
 ?>
