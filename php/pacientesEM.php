86c41302766cf81ec7d49d471a0de9af6eab8846
jhernandez19 <50375122+jhernandez19@users.noreply.github.com>
2019-05-29 11:35:31 -0500

    Update pacientesEM.php

--- php/pacientesEM.php
+++ php/pacientesEM.php
@@ -1,9 +1,9 @@
 <?php
-
 $conn = mysqli_connect('localhost','root','','tfidoctor');
 
+$doctor = $_GET['doctor'];
 
-$resultado = mysqli_query($conn,"SELECT * FROM examenes_m");
+$resultado = mysqli_query($conn,"SELECT * FROM examenes_m where doctor = '$doctor'");
 
 $data = array();
 
